f2db2bc5932fda87186ee1529472bd460852d0a2
Akramtheking <163847855+Akramtheking@users.noreply.github.com>
2025-03-17 15:17:48 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <div className="max-w-xl m-auto bg-gray-200 mt-20">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
